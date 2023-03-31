621ed0ba6785b30e73d382565a6707dde08d33dc
Anderson Oliveira da Silva <anderson.os88@hotmail.com>
2023-03-30 21:27:56 -0300

    api linguagens

---
+++ src/main/java/com/anderson/apilinguagens/LinguagemRepository.java
@@ -0,0 +1,7 @@
+package com.anderson.apilinguagens;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository  extends MongoRepository<Linguagem, String> {
+
+}
---
+++ src/test/java/com/anderson/apilinguagens/ApiLinguagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.apilinguagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLinguagensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
