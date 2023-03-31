a393182daeb1375508286b85ff337c3d588396a1
Anderson Oliveira da Silva <anderson.os88@hotmail.com>
2023-03-31 19:08:24 -0300

    postmapping

--- src/main/java/com/anderson/apilinguagens/LinguagemController.java
+++ src/main/java/com/anderson/apilinguagens/LinguagemController.java
@@ -2,6 +2,8 @@ package com.anderson.apilinguagens;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,6 +18,12 @@ public class LinguagemController {
     public List<Linguagem> obterLinguagens() {
         List<Linguagem> linguagens = repository.findAll();
         return linguagens;
+    }
+
+    @PostMapping("/linguagens")
+    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
+        Linguagem linguagemSalva = repository.save(linguagem);
+        return linguagemSalva;
     }
 
 
